e7af2979f06993894410e4286019ba236b91b215
OleksandrDanchuk <117369013+OleksandrDanchuk@users.noreply.github.com>
2023-04-25 18:23:53 +0300

    begin

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 import './css/styles.css';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import debounce from 'lodash.debounce';
+
+import { fetchCountries } from './fetchCountries';
+import { renderListCountries } from './rendering';
+import { rendercountry } from './rendering';
 
 const DEBOUNCE_DELAY = 300;
+
+const refs = {
+  input: document.querySelector('#search-box'),
+  countryList: document.querySelector('.country-list'),
+  countryInfo: document.querySelector('.country-info'),
+};
